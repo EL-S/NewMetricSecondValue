d612c007b78d2fe4b938d5964566f0fb9c05f777
EL-S <31227083+EL-S@users.noreply.github.com>
2018-09-25 22:59:46 +1000

    Backwards Calculation
    Modified to attempt to work backwards instead of forwards for efficiency

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ real_minute = real_second * 60
 real_hour = real_minute * 60
 real_day = real_hour * 24
 real_month = real_day * 30
-real_year = real_day * 365
+real_year = real_day * 365.2425
 
 s = 100
 m = 100
@@ -37,18 +37,32 @@ print(new1,new2,new3)
 
 print(real_year)
 
+var = False
 
 print(second,new_second1)
 print(minute,new_second2)
 print(hour,new_second3)
 print(day,new_second4)
 
-for i in range(1,200):
-    for j in range(1,200):
-        for k in range(1,100):
-            for z in range(1,1000):
+for i in range(10,11):
+    if float(real_day/i).is_integer():
+        for j in range(1,1000):
+            if ((real_day/i)/j).is_integer():
+                print("Hours:",i,"Seconds:",real_day/i,"Minutes:",j,"Seconds:",(real_day/i)/j)
+                value = (100/j)*((real_day/i)/j)
+                if value.is_integer():
+                    print(value)
+                #if (100/((real_day/i)/j)*j).is_integer():
+                    #print((100/((real_day/i)/j)),int(1/(100/((real_day/i)/j))*j))
+if var:
+    for i in range(1,200):
+        for j in range(1,200):
+            for k in range(1,100):
+                z = 365.2425
                 value = (i*j*k*z)/real_year
                 if float(value).is_integer():
                     print(i,j,k,z,value)
+
 print("done")
+
 
